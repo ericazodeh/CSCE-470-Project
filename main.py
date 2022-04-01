54dd604d36058a10a85691c199752251c5162f28
ericazodeh <88304433+ericazodeh@users.noreply.github.com>
2022-04-01 07:59:47 -0500

    added UI functionality

--- main.py
+++ main.py
@@ -5,23 +5,65 @@ import csv
 import KNN_Predict
 import pickle
 
+
 print("Running")
 app = Flask(__name__)
+
+with open('data/training_pkl', 'rb') as f:
+        training_vectors= pickle.load(f)
+    
+with open('data/word_bank_pkl', 'rb') as f:
+    Words= pickle.load(f)
 
 
 @app.route("/")
 def index():
-    
-    with open('data/training_pkl', 'rb') as f:
-        training_vectors= pickle.load(f)
-    
-    with open('data/word_bank_pkl', 'rb') as f:
-        Words= pickle.load(f)
+    return render_template("index.html")
 
-    # Enter the query instead of "many years jerr"
-    movies=KNN_Predict.predict("many years jerr",training_vectors,Words)
+@app.route('/', methods=['POST'])
+def pickMovies():
     
-    return movies
+    textBoxInput = request.form['text']
+    # Enter the query instead of "many years jerr"
+    movies=KNN_Predict.predict(textBoxInput,training_vectors,Words)
+    #return render_template("index.html")
+    i = 1
+    moviePicks ={
+        "first" : {"" : 0.0},
+        "second" : {"" : 0.0},
+        "third" : {"" : 0.0},
+        "fourth" : {"" : 0.0},
+        "fifth" : {"" : 0.0}
+    }
+    firstMovie = secondMovie = thirdMovie = fourthMovie = fifthMovie = "No movie prediction"
+    for movie, value in sorted(movies.items(), key = lambda x: x[1]):
+        if i == 1:
+            #moviePicks["first"] = {movie : value}
+            firstMovie = movie
+        elif i == 2:
+            #moviePicks["second"] = {movie : value}
+            secondMovie = movie
+        elif i == 3:
+            #moviePicks["third"] = {movie : value}
+            thirdMovie = movie
+        elif i == 4:
+            #moviePicks["fourth"] = {movie : value}
+            fourthMovie = movie
+        elif i == 5:
+            #moviePicks["fifth"] = {movie : value}
+            fifthMovie = movie
+        
+        i += 1
+
+    hi = "hi"
+    return render_template("movies.html",movies = movies, firstMovie=firstMovie, secondMovie = secondMovie,thirdMovie = thirdMovie,fourthMovie = fourthMovie, fifthMovie = fifthMovie)
+
+@app.route('/movies', methods=['POST'])
+def getMovieInfo():
+    return "This part is not done yet"
+
+
+
   
 
 if __name__ == "__main__":
